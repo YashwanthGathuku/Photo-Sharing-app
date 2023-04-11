07668e4798d1070396b885ea3d1a1d1e84d34bb2
Sphinxlike <yashwanth8035@gmail.com>
2023-04-11 08:38:30 -0400

    nodemon

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = new express;
 const port = 1311;
+app.use(express.static('public'))
 
 app.listen(port, ()=>{
     console.log(`serving photosharing app on http://localhost:${port}`);
