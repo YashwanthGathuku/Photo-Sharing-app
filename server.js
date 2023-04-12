3f2d46d1f9a5aec98103d4c6d31462976506d7e2
Sphinxlike <yashwanth8035@gmail.com>
2023-04-12 15:35:33 -0400

    .ejsfiles

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = new express;
 const port = 1311;
 app.use(express.static('public'))
+app.set("view engine", "ejs")
 
 app.listen(port, ()=>{
     console.log(`serving photosharing app on http://localhost:${port}`);
+})
+app.get("/", (request,response)=>{
+    response.render('index');
 })
